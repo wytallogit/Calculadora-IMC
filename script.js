a856fb0011380d31b37899c86b83279a931d127a
Wytallo Vinicius <157238340+wytallogit@users.noreply.github.com>
2024-07-17 15:01:58 -0300

    Update

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcular() {
+    let txtidade = document.getElementById('txtidade')
+    let txtalt = document.getElementById('txtalt')
+    let txtps = document.getElementById('txtpes')
+
+
+    let sexo = ''
+    if (document.getElementById('mas').checked) {
+        sexo = 'Masculino'
+    } else {
+        sexo = 'Feminino'
+    }
+    
+    let idade = Number(txtidade.value)
+    let altura = Number(txtalt.alt)
+    let peso = Number(txtps.value)
+
+
+    function caulculoIMC() {
+        return 
+    }
+
+}
+
+
